045fa2fd68943de4d598304794b8b7aefd629f51
NonoLeRobot <sannier.renaud@gmail.com>
2023-02-09 21:03:02 +0100

    refactoring code

---
+++ js/classes/building.js
@@ -0,0 +1,40 @@
+class Building {
+  constructor({ position = { x: 0, y: 0 } }) {
+    this.position = position;
+    this.width = 64 * 2;
+    this.height = 64;
+    this.center = {
+      x: this.position.x + this.width / 2,
+      y: this.position.y + this.height / 2,
+    };
+    this.projectiles = [];
+    this.radius = 250;
+    this.target;
+    this.frames = 0;
+    this.fireRate = 25;
+  }
+  draw() {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(this.position.x, this.position.y, this.width, 64);
+    ctx.beginPath();
+    ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "rgba(0,0,255,0.1)";
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    if (this.frames % this.fireRate === 0 && this.target) {
+      this.projectiles.push(
+        new Projectile({
+          position: {
+            x: this.center.x,
+            y: this.center.y,
+          },
+          enemy: this.target,
+        })
+      );
+    }
+    this.frames++;
+  }
+}
---
+++ js/classes/projectiles.js
@@ -0,0 +1,31 @@
+class Projectile {
+  constructor({ position = { x: 0, y: 0 }, enemy }) {
+    this.position = position;
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.enemy = enemy;
+    this.radius = 10;
+    projectiles.push(this);
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "orange";
+    ctx.fill();
+  }
+  update() {
+    this.draw();
+    const angle = Math.atan2(
+      this.enemy.center.y - this.position.y,
+      this.enemy.center.x - this.position.x
+    );
+    const power = 20;
+    this.velocity.x = Math.cos(angle) * speedProjectiles;
+    this.velocity.y = Math.sin(angle) * speedProjectiles;
+
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
